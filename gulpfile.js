19b7bbcb004585d79adc1b78daed06b939da9bf9
Moey Design <moeydesign@Mortars-iMac.local>
2017-12-04 15:09:40 -0500

    installed gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+
+gulp.task('js', function () {
+    gulp.src('src/index.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+    }
+);
+
+gulp.task('default', ['js'])
