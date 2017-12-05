8b8a624e621cc05c1e8e23f4b9ac2cb9afd4d4ae
Moey Design <moeydesign@Mortars-iMac.local>
2017-12-05 14:55:14 -0500

    changed name from index.js -> plugin.js

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 
-import VueSimpleSVG from '../dist/index.js'
+import VueSimpleSVG from '../dist/plugin.js'
 Vue.use(VueSimpleSVG)
-
-// Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function () {
-    gulp.src('src/index.js')
+    gulp.src('src/plugin.js')
         .pipe(babel({
             presets: ['env']
         }))
